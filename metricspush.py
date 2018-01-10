e8b4a99d1567e87e761c3881a17d5d8336a25ca8
Adam Sandor <adam.sandor@live.com>
2018-01-10 12:32:07 +0100

    add documentation

--- metricspush.py
+++ metricspush.py
@@ -9,31 +9,39 @@ metadata_header = {'Metadata-Flavor': 'Google'}
 
 
 def instance_id():
+    # Get the instance_id from the GCP metadata API
     r = requests.get("http://metadata.google.internal./computeMetadata/v1/instance/id", headers=metadata_header)
     return r.text
 
 
 def zone():
+    # Get the zone from the GCP metadata API
     r = requests.get("http://metadata.google.internal./computeMetadata/v1/instance/zone", headers=metadata_header)
     parts = r.text.split('/')
     return parts[len(parts) - 1]
 
 
 def cluster_name():
+    # Get the cluster name from an environment variable. This will be set from the ConfigMap cluster-metadata (see: clustermetadata.yaml)
+    # This is just a custom solution, as there is no API to determine this value from a running Pod.
     return os.environ['CLUSTER_NAME'] # cannot be determined from metadata
 
 
 def container_name():
+    # The container name is passed in from the yaml file as an environment variable.
     return os.environ['CONTAINER_NAME'] # cannot be determined from metadata (HOSTNAME is Pod ID)
 
 
 def namespace_id():
+    # Get namespace_id from the Kubernetes API server. The Pod has to have the proper permissions to access this information
+    # if you have enabled RBAC on your cluster.
     config.load_incluster_config()
     v1 = client.CoreV1Api()
     return v1.list_namespace().items[0].metadata.uid
 
 
 def pod_uid():
+    # The UID of the Pod is passed in as an environment variable using the Kubernetes Downward API (see: metricspod.yaml)
     return os.environ['POD_UID']
 
 
